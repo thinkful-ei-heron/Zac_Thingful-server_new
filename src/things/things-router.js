decf6bbf8b7d17e003fc5163e18073bdfa64d548
Zacharia Lutz <zacharia.lutz@gmail.coom>
2020-02-05 18:14:15 -0600

    login api updated

--- src/things/things-router.js
+++ src/things/things-router.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const ThingsService = require('./things-service')
-const { requireAuth } = require('../middleware/jwt-auth')
+'use strict';
 
-const thingsRouter = express.Router()
+const express = require('express');
+const ThingsService = require('./things-service');
+const { requireAuth } = require('../middleware/jwt-auth');
+
+const thingsRouter = express.Router();
 
 thingsRouter
   .route('/')
   .get((req, res, next) => {
     ThingsService.getAllThings(req.app.get('db'))
       .then(things => {
-        res.json(things.map(ThingsService.serializeThing))
+        res.json(things.map(ThingsService.serializeThing));
       })
-      .catch(next)
-  })
+      .catch(next);
+  });
 
 thingsRouter
   .route('/:thing_id')
   .all(requireAuth)
   .all(checkThingExists)
   .get((req, res) => {
-    res.json(ThingsService.serializeThing(res.thing))
-  })
+    res.json(ThingsService.serializeThing(res.thing));
+  });
 
 thingsRouter.route('/:thing_id/reviews/')
   .all(requireAuth)
@@ -31,29 +33,28 @@ thingsRouter.route('/:thing_id/reviews/')
       req.params.thing_id
     )
       .then(reviews => {
-        res.json(reviews.map(ThingsService.serializeThingReview))
+        res.json(ThingsService.serializeThingReviews(reviews));
       })
-      .catch(next)
-  })
+      .catch(next);
+  });
 
-/* async/await syntax for promises */
 async function checkThingExists(req, res, next) {
   try {
     const thing = await ThingsService.getById(
       req.app.get('db'),
       req.params.thing_id
-    )
+    );
 
     if (!thing)
       return res.status(404).json({
         error: `Thing doesn't exist`
-      })
+      });
 
-    res.thing = thing
-    next()
+    res.thing = thing;
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
-module.exports = thingsRouter
+module.exports = thingsRouter;
